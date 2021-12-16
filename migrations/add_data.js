3c4ea7be6d756302be2d8109f247c9f1e183cf4e
Zhen <g.hijack333@gmail.com>
2021-12-16 10:37:51 +0800

    update add_data.js

--- migrations/add_data.js
+++ migrations/add_data.js
@@ -10,19 +10,17 @@ const pool = new Pool({
   password: "admin",
   port: "5432",
 });
-var queryUser = `INSERT INTO users(
+const queryUser = `INSERT INTO users(
   name, profileId) VALUES(
   '', '${profileid}'
   );`;
 
-var queryHistory = `INSERT INTO history(exchangeTime, coinSymbol,balance,profileId) VALUES`;
-let historyValues = ``;
-history.forEach((item) => {
+let queryHistory = `INSERT INTO history(exchangeTime, coinSymbol,balance,profileId) VALUES`;
+const historyValues = history.map((item) => {
   const exchangetime = Math.floor(new Date(item.exchangetime).getTime() / 1000);
-
-  historyValues += `(${exchangetime}, '${item.coinsymbol}', ${item.balance}, '${profileid}'),`;
+  return `(${exchangetime}, '${item.coinsymbol}', ${item.balance}, '${profileid}')`;
 });
-let stringWithoutComma = historyValues.replace(/,*$/, ";");
+const stringWithoutComma = historyValues.join(",");
 const finalHistory = queryHistory + stringWithoutComma;
 pool.query(queryUser + finalHistory, (err, res) => {
   console.log(err, res);
