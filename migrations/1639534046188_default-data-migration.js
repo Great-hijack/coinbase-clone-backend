60649ff751b7ff833fc233e76ea8877652e15751
Zhen <g.hijack333@gmail.com>
2021-12-18 12:02:38 +0800

    update database again

--- migrations/1639534046188_default-data-migration.js
+++ migrations/1639534046188_default-data-migration.js
@@ -1,14 +1,15 @@
 /* eslint-disable camelcase */
-let historyValue = `INSERT INTO "public"."history" VALUES (1, 1638689108, 'ETH', 0.3, '75fi73Fk');
-INSERT INTO "public"."history" VALUES (2, 1636374120, 'ETH', 0.12, '75fi73Fk');
-INSERT INTO "public"."history" VALUES (3, 1636152840, 'ETH', -0.02, '75fi73Fk');
-INSERT INTO "public"."history" VALUES (4, 1636146240, 'BTC', 0.012, '75fi73Fk');`;
-let userValue = `INSERT INTO "public"."users" VALUES (1, 'test', '75fi73Fk');`;
+let historyValue = `INSERT INTO "public"."history"(exchangetime, coinsymbol,balance,profileid) VALUES (1638689108, 'ETH', 0.3, '75fi73Fk');
+INSERT INTO "public"."history"(exchangetime, coinsymbol,balance,profileid) VALUES (1636374120, 'ETH', 0.12, '75fi73Fk');
+INSERT INTO "public"."history"(exchangetime, coinsymbol,balance,profileid) VALUES (1636152840, 'ETH', -0.02, '75fi73Fk');
+INSERT INTO "public"."history"(exchangetime, coinsymbol,balance,profileid) VALUES (1636146240, 'BTC', 0.012, '75fi73Fk');`;
+let userValue = `INSERT INTO "public"."users"(
+  name, profileid) VALUES ('test', '75fi73Fk');`;
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  // pgm.db.query(userValue);
-  // pgm.db.query(historyValue);
+  pgm.db.query(userValue);
+  pgm.db.query(historyValue);
 };
 
 exports.down = (pgm) => {};
